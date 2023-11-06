ef19c90a23fb5886edf40839bf15d2594d494195
Roger Correa <j.rogercorreamello@gmail.com>
2023-11-06 17:19:44 -0300

    primeiras configurações

--- CWallet.c
+++ CWallet.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
 
+#ifdef _WIN32
+    #define CLEAR_SCREEN "cls"
+#else 
+    #define CLEAR_SCREEN "clear"
+#endif
+
+int main() {
+    
+}
